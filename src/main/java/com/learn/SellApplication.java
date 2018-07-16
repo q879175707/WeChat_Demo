a7f0b553c2318c528c4c13d60ffd3d7f16bd0a89
秦旭亮 <36783625+q879175707@users.noreply.github.com>
2018-07-16 12:29:29 +0800

    Update SellApplication.java
    解决冲突做准备

--- src/main/java/com/learn/SellApplication.java
+++ src/main/java/com/learn/SellApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SellApplication {
 
+        // 我在这里做了改动，一会解决冲突
 	public static void main(String[] args) {
 		SpringApplication.run(SellApplication.class, args);
 	}
