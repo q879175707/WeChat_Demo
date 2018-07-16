1fa322d16c955c23d4abe7be9ec6a6d9f4cb57f4
qin <879175707@qq.com>
2018-07-16 12:41:55 +0800

    解决冲突

--- src/main/java/com/learn/SellApplication.java
+++ src/main/java/com/learn/SellApplication.java
@@ -8,6 +8,7 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 @EnableRedisRepositories
 public class SellApplication {
 
+        // 我在这里做了改动，一会解决冲突
 	public static void main(String[] args) {
 		SpringApplication.run(SellApplication.class, args);
 	}
